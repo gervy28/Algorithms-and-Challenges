ed8e785e6155762e8383fd5a8c58a78e03701e21
Nate Gervenak <gervenaknate@gmail.com>
2022-09-12 21:30:18 -0500

    updated insertion short

--- algorithms/SortingInsertion.js
+++ algorithms/SortingInsertion.js
@@ -1,10 +1,20 @@
 function insertionSort(arr){  
   for(let i = 1; i < arr.length; i++){
+
       let currentNum = arr[i];
+
       for(let j = i-1; j >= 0 && arr[j] > currentNum; j--){
           arr[j+1] = arr[j];
+          arr[j] = currentNum;
       }
-      arr[j+1] = currentNum;
   }
+
+  return arr;
 }
-insertionSort([2, 13, 4, 7, 77, 21, 1])
+
+
+console.log(insertionSort([2, 13, 4, 7, 77, 21, 1]));
+
+console.log( insertionSort([6, 3, 18, 0, 99, 100, 32, 45, 12, 8, 65, 54]) );
+
+console.log( insertionSort([ 0, 3, 4, 4, 8]) );
